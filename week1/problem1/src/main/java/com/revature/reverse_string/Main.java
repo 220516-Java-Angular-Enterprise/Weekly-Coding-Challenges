f85d21cbac7e63e25c6ed7a62ad00b947dca12b2
Hal Coronado <hallie_coronado1@baylor.edu>
2022-05-20 10:04:13 -0500

    cool

--- week1/problem1/src/main/java/com/revature/reverse_string/Main.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/Main.java
@@ -1,4 +1,13 @@
 package com.revature.reverse_string;
 
 public class UI {
+    Solution sol = new Solution();
+
+    sol.reverseString("Hello");
+    sol.reverseString("Hello World!");
+    sol.reverseString("12345");
+    sol.reverseString("!@#$%");
+    sol.reverseString("");
+    sol.reverseString(".");
+
 }
--- week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
@@ -2,6 +2,11 @@ package com.revature.reverse_string;
 
 public class Solution {
     public String reverseString(String s) {
+
+        StringBuilder phrase = new StringBuilder();
+        phrase.append(s);
+        phrase.reverse();
+        System.out.println(phrase);
         throw new RuntimeException("TODO! Needs implementation...");
     }
 }

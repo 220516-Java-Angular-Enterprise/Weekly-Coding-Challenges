c39e48e41ff98ca5c84577ccf60b88d1d687a216
despres99 <ethan.despres.99@gmail.com>
2022-05-20 11:06:39 -0400

    Finished Coding Challenge

--- week1/problem2/src/main/java/com/revature/abracadabra/Solution.java
+++ week1/problem2/src/main/java/com/revature/abracadabra/Solution.java
@@ -1,9 +1,19 @@
 package com.revature.abracadabra;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<String> abracadabra(int n) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        List<String> list = new ArrayList<String>(n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) list.add("abracadabra");
+            else if (i % 3 == 0) list.add("abraca");
+            else if (i % 5 == 0) list.add("dabra");
+            else list.add(Integer.toString(i));
+        }
+
+        return list;
     }
 }
--- week1/problem3/src/main/java/com/revature/reverse_int/Solution.java
+++ week1/problem3/src/main/java/com/revature/reverse_int/Solution.java
@@ -2,6 +2,16 @@ package com.revature.reverse_int;
 
 public class Solution {
     public int reverseInt(int n) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        String rev = "";
+        String nStr = Integer.toString(n);
+
+        if (nStr.charAt(0) == '-') rev += '-';
+
+        for (int i = nStr.length()-1; i >= 0; i--) {
+            if (nStr.charAt(i) != '-') rev += nStr.charAt(i);
+        }
+
+        n = Integer.parseInt(rev);
+        return n;
     }
 }

c5b1e74dd4546f362d14f37d769e2ed45415ea14
William Marquardt <william940@revature.net>
2022-05-20 10:22:56 -0500

    Completed

---
+++ week1/problem1/src/main/java/com/revature/reverse_string/UI.java
@@ -0,0 +1,9 @@
+package com.revature.reverse_string;
+
+public class Main {
+    public static void main (String[] args){
+        String testStr="Herp";
+        System.out.println(testStr.substring(1,1));
+    }
+
+}

16ed8f02503e96141940af9787d48ac5115bd77d
ML <labrecquemegan@hotmail.com>
2022-05-20 11:27:46 -0400

    prob 2 not working but 5 min warning

--- week1/problem2/src/main/java/com/revature/abracadabra/Main.java
+++ week1/problem2/src/main/java/com/revature/abracadabra/Main.java
@@ -1,4 +1,11 @@
 package com.revature.abracadabra;
 
 public class Main {
+
+public static void main(String[] args) {
+    Solution sol = new Solution();
+
+    System.out.println("Your new array is: " + sol.abracadabra(15));
+}
+
 }
--- week1/problem2/src/main/java/com/revature/abracadabra/Solution.java
+++ week1/problem2/src/main/java/com/revature/abracadabra/Solution.java
@@ -1,9 +1,45 @@
 package com.revature.abracadabra;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+
+
     public List<String> abracadabra(int n) {
-        throw new RuntimeException("TODO! Needs implementation...");
+
+//        Create variables to hold info
+        int i = 0;
+        String s = "";
+
+//        create try catch for exception
+        List<String> newArr = new ArrayList();
+//        Create a for loop to compare given int with 0-given int and convert to array
+        for (i = 0; i == n; i++) {
+//            if i is less than n, add I to an array at the next index.
+
+            if (i < n) {
+
+                if (i % 3 == 0) {
+                    s = "abraca";
+                    newArr.add(s);
+
+                } else if (i % 5 == 0) {
+                    s = "abraca";
+                    newArr.add(s);
+
+                } else if (i % 5 == 0 && i % 3 == 0) {
+                    s = "abraca";
+                    newArr.add(s);
+
+                } else
+                    s = String.valueOf(i);
+                newArr.add(s);
+            }
+
+
+//        throw new RuntimeException("TODO! Needs implementation...");
+        }
+        return newArr;
     }
 }

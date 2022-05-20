8330688b0ecefcdbf0b4b54375d77873ead6f99a
ML <labrecquemegan@hotmail.com>
2022-05-20 11:30:41 -0400

    challenge over

--- week1/problem3/src/main/java/com/revature/reverse_int/Main.java
+++ week1/problem3/src/main/java/com/revature/reverse_int/Main.java
@@ -1,4 +1,11 @@
 package com.revature.reverse_int;
 
+import com.revature.abracadabra.Solution;
+
 public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println("Your new number is: " + sol.reverseInt(123));
+    }
 }
--- week1/problem3/src/main/java/com/revature/reverse_int/Solution.java
+++ week1/problem3/src/main/java/com/revature/reverse_int/Solution.java
@@ -2,6 +2,12 @@ package com.revature.reverse_int;
 
 public class Solution {
     public int reverseInt(int n) {
+
+//        variable to hold reversed number
+        int reversed = 0;
+
+//        create for loop
+
         throw new RuntimeException("TODO! Needs implementation...");
     }
 }

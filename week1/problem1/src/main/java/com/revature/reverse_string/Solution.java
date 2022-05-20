43c3211ac4ffdc4696eaada2b48c4fd3ec0f6375
trknox <love.lala2003@gmail.com>
2022-05-20 11:32:53 -0400

    Completed coding challenge

--- week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
@@ -1,7 +1,17 @@
 package com.revature.reverse_string;
 
 public class Solution {
+    String newS = "";
+    int count = 1;
     public String reverseString(String s) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        //base case
+        if(count == s.length()){
+            return s;
+        }else {
+            count += 1;
+            newS = s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
+            reverseString(newS);
+            return newS;
+        }
     }
 }
--- week1/problem2/src/main/java/com/revature/abracadabra/Main.java
+++ week1/problem2/src/main/java/com/revature/abracadabra/Main.java
@@ -1,4 +1,9 @@
 package com.revature.abracadabra;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().abracadabra(3));
+    }
 }

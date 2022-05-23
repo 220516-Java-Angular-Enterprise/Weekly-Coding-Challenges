c818b7176ef2cb3eff909deb9535473dea2319c9
Mbq24 <92138004+Mbq24@users.noreply.github.com>
2022-05-23 08:15:55 -0600

    Update Problem.java

--- week1/untitled1/src/main/java/com/resources/assignments/problem1/Problem.java
+++ week1/untitled1/src/main/java/com/resources/assignments/problem1/Problem.java
@@ -3,10 +3,13 @@ package main.java.com.resources.assignments.problem1;
 public class Problem {
 
     public StringBuilder reverseString(String s) {
-        StringBuilder reverse = new StringBuilder();
-
-        reverse.append(s);
-        reverse.reverse();
+        String reverse = "";
+//        StringBuilder reverse = new StringBuilder();
+//        reverse.append(s);
+//        reverse.reverse();
+        for (int i=s.length()-1; i>=0; i--) {
+            reverse += (s.charAt(i));
+        }
 
         return reverse;
     }

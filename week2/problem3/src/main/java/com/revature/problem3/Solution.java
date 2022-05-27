26864fd2ae1437ac1cc32db003a71b4e05b147ae
ML <labrecquemegan@hotmail.com>
2022-05-27 11:16:23 -0400

    sol 3 final push

--- week2/problem3/src/main/java/com/revature/problem3/Solution.java
+++ week2/problem3/src/main/java/com/revature/problem3/Solution.java
@@ -8,13 +8,14 @@ public class Solution {
 //        need to split array by spaces
         String[] words = sentence.split(" ");
 
-//        set a var for count
+//        set a var for count/ans
         int count = 0;
-        String ans = ("" + count);
+        String ans = "";
 
 //        create for loop to go through new array
 //        only do one word, need to look at a[i] compare to whole string, then a[i+1] etc...
         for (int i = 0; i <words.length; i++) {
+            ans = (words[i] + count);
             if (words.equals(words[i])) {
                 count ++;
             }

c44db43ef1b3d54f1ecf8ce3abb90a40604b5ac3
trknox <love.lala2003@gmail.com>
2022-05-27 10:24:27 -0400

    added week 2

--- week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/Solution.java
@@ -1,5 +1,21 @@
 package com.revature.reverse_string;
 
+/* public class Solution {
+    String newS = "";
+    int count = 0;
+    public void reverseString(String s) {
+        //base case
+        if(s.length() <= 1){
+            System.out.println(s);
+        }else {
+            //print last element then call method again with the string not including that last element
+            System.out.print(s.charAt(s.length() - 1));
+            reverseString(s.substring(0, s.length() - 1));
+        }
+    }
+}
+*/
+
 public class Solution {
     String newS = "";
     int count = 1;
--- week1/problem1/src/main/java/com/revature/reverse_string/UI.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/UI.java
@@ -3,6 +3,6 @@ package com.revature.reverse_string;
 public class UI {
     public static void main(String[] args) {
         //declaring new object without assigning to variable as I'm only using it once
-        System.out.println(new Solution().reverseString("string"));
+        new Solution().reverseString("string");
     }
 }

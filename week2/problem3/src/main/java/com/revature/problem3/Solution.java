f763eabe99a000dcff7a0b1a0bf06eca999e86d4
igeet <105670057+igeet@users.noreply.github.com>
2022-05-27 10:26:34 -0500

    Update Solution.java

--- week2/problem3/src/main/java/com/revature/problem3/Solution.java
+++ week2/problem3/src/main/java/com/revature/problem3/Solution.java
@@ -2,8 +2,25 @@ package com.revature.problem3;
 
 import java.util.*;
 
-public class Solution {
-    public String wordCount(String sentence) {
-        throw new RuntimeException("TODO! Needs implementation...");
+public class Solution 
+{
+    public String wordCount(String sentence) 
+    {
+        int count = 0;
+        String word[] = sentence.split(" ");
+
+        for(int i = 0; i < word.length; i++)
+        {
+            for(int j = 0; j < word.length; j++)
+            {
+                if(word[i].equals(word[j]))
+                {
+                    count++;
+                }
+
+            }
+        }
     }
 }
+        return null;
+    

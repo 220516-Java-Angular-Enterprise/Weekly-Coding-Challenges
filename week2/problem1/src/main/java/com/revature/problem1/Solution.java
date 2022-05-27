d4e93b781af3c474cea8db58c5759c85a03e587e
igeet <105670057+igeet@users.noreply.github.com>
2022-05-27 10:10:10 -0500

    Update Solution.java

--- week2/problem1/src/main/java/com/revature/problem1/Solution.java
+++ week2/problem1/src/main/java/com/revature/problem1/Solution.java
@@ -2,6 +2,9 @@ package com.revature.problem1;
 
 public class Solution {
     public int elementNotInArray(int[] arr, int k) {
+        Array.sort(arr);
+
+        return arr[k-1];
         throw new RuntimeException("TODO! Needs implementation...");
     }
 }

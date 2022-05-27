cb07bf676b64801cff898a962eacdc35d1457244
Pelin Akbiyik <pakbiyik@bu.edu>
2022-05-27 07:34:07 -0400

    updates

--- week1/problem1/src/main/java/com/revature/reverse_string/Main.java
+++ week1/problem1/src/main/java/com/revature/reverse_string/Main.java
@@ -1,12 +1,12 @@
-//package com.revature.reverse_string;
+package com.revature.reverse_string;
 
-//public class Main {
+public class Main {
 
-//public static void main(String[] args){
-   // Solution1 sol=new Solution1();
-    //System.out.println(sol.reverseString("Hello World"));
+public static void main(String[] args){
+   Solution1 sol=new Solution1();
+    System.out.println(sol.reverseString("Hello World"));
 
 
 
-//}
-//}*\
+}
+}

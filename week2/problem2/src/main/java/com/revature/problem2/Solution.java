33d163aa08c46f9ad08ccba1bc571db28247a6b0
igeet <105670057+igeet@users.noreply.github.com>
2022-05-27 10:21:35 -0500

    Update Solution.java

--- week2/problem2/src/main/java/com/revature/problem2/Solution.java
+++ week2/problem2/src/main/java/com/revature/problem2/Solution.java
@@ -2,8 +2,13 @@ package com.revature.problem2;
 
 import java.util.List;
 
-public class Solution {
-    public List<String> autoComplete(List<String> list) {
-        throw new RuntimeException("TODO! Needs implementation...");
+public class Solution 
+{
+    public List<String> autoComplete(List<String> list)
+    {
+         for (int i = 0; i < list.size(); i++ )
+         {
+            for (int j = i + 1; j < list.)
+         }
     }
 }

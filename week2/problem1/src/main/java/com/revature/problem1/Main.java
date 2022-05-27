2333853d69fda748196fb82b12e92ba8ba796fef
Hal Coronado <hallie_coronado1@baylor.edu>
2022-05-27 11:16:05 -0500

    close

---
+++ week2/problem1/src/main/java/com/revature/problem1/Main.java
@@ -0,0 +1,12 @@
+package com.revature.problem1;
+
+import com.revature.problem1.Solution;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sol1 = new Solution();
+    }
+
+
+
+}
---
+++ week2/problem1/src/main/java/com/revature/problem1/Solution.java
@@ -0,0 +1,33 @@
+package com.revature.problem1;
+
+public class Solution {
+    public int elementNotInArray(int[] arr, int k) {
+        int maxNum = 0;
+        int minNum = 0;
+        int newMinNum = 0;
+        int notInArray = 0;
+
+        for (int x: arr){
+            if (arr[x] >= maxNum){
+                maxNum = arr[x];
+            }else if (arr[x] <= maxNum){
+                arr[x] = newMinNum;
+                if(minNum >= newMinNum){
+                    minNum= newMinNum;
+                }
+            }
+        }
+        for(int i = minNum; i < maxNum; i++){
+            while(i<= k){
+                continue;
+            }
+            if(i<maxNum && i>k){
+                notInArray = i;
+            }else return 0;
+        }
+
+
+return notInArray;
+        //throw new RuntimeException("TODO! Needs implementation...");
+    }
+}

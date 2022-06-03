9a458f24b1563628f2f3b9c5026390efe4a3d6c1
Jeff Sturgeon <jeff637@revature.net>
2022-06-03 07:54:31 -0700

    freebies progress

--- week3/freebie/src/main/java/com/revature/problem/Solution.java
+++ week3/freebie/src/main/java/com/revature/problem/Solution.java
@@ -2,6 +2,13 @@ package com.revature.problem;
 
 public class Solution {
     public String deleteAllNumbers(String s) {
+
+        String noNum;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (equals(s[i]))
+        }
+
         throw new RuntimeException("TODO! Needs implementation...");
     }
 }

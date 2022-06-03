b58efb8c9f0ab59bdd0bd28eb900660ba6073d69
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-06-03 10:07:37 -0400

    week 2 stuff

---
+++ week2/problem3/src/main/java/com/revature/problem3/MyClassIG.java
@@ -0,0 +1,8 @@
+package com.revature.problem3;
+
+public class MyClassIG {
+
+    public boolean Foo(){
+        return true;
+    }
+}
---
+++ week2/problem3/src/test/java/com/revature/problem3/MyClassIGTest.java
@@ -0,0 +1,7 @@
+package com.revature.problem3;
+
+import junit.framework.TestCase;
+
+public class MyClassIGTest extends TestCase {
+
+}

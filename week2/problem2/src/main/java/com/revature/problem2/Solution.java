d23df2cabbd6c2ba6f480c74818b323af26bc113
bduong0929 <bao.duong@revature.com>
2022-06-03 08:27:35 -0600

    added week3 coding challenges

--- week2/problem2/src/main/java/com/revature/problem2/Solution.java
+++ week2/problem2/src/main/java/com/revature/problem2/Solution.java
@@ -7,6 +7,26 @@ import java.util.List;
 
 public class Solution {
     public List<String> autoComplete(List<String> list) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        //Collections has a sort method.  Let's use that.
+        List<String> sortedList = list.subList(1,list.size());
+        Collections.sort(sortedList);
+        //now we can use the input to search through the list.
+        List<String> outList = new ArrayList<String>();
+        String inStr = list.get(0);
+        for (String s : sortedList){
+            //Make sure we're not comparing to a string that's too long:
+            //It took me half an hour to figure out that I put '>' instead of '<'
+
+            if (s.length()>=inStr.length()) {
+                //if the first inStr.length() chars in substring match, add to output
+
+                if (s.substring(0, inStr.length()).contains(inStr)) {
+
+                    outList.add(s);
+                }
+            }
+        }
+
+        return outList;
     }
 }

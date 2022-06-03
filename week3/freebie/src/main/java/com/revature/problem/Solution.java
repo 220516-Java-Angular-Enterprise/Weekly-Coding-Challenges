d0e70155311df42c0e1c54d54dd87196035a16a0
Nouman6206 <93328225+Nouman6206@users.noreply.github.com>
2022-06-03 10:41:44 -0500

    added coding challenges

--- week3/freebie/src/main/java/com/revature/problem/Solution.java
+++ week3/freebie/src/main/java/com/revature/problem/Solution.java
@@ -2,6 +2,17 @@ package com.revature.problem;
 
 public class Solution {
     public String deleteAllNumbers(String s) {
-        throw new RuntimeException("TODO! Needs implementation...");
+
+        String result = "";
+        char[] ch = s.toCharArray();
+
+        for(int i =0; i < ch.length; i++){
+            if (!Character.isDigit(ch[i])) {
+                result = result + ch[i];
+
+            }
+        }
+        return result;
+
     }
 }
--- week3/problem1/src/main/java/com/revature/problem/Solution.java
+++ week3/problem1/src/main/java/com/revature/problem/Solution.java
@@ -2,6 +2,6 @@ package com.revature.problem;
 
 public class Solution {
     public int validWords(String sentence) {
-        throw new RuntimeException("TODO! Needs implementation...");
+
     }
 }

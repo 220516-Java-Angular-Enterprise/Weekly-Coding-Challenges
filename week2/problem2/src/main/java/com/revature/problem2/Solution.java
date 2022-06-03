55898e20bbb6adb81503b537273eb9643ae68f12
Phutondog <ericmphu@gmail.com>
2022-06-03 10:29:42 -0400

    message

--- week2/problem2/src/main/java/com/revature/problem2/Solution.java
+++ week2/problem2/src/main/java/com/revature/problem2/Solution.java
@@ -1,5 +1,6 @@
 package com.revature.problem2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
@@ -7,8 +8,18 @@ public class Solution {
 
         // iterate through list
         // go through each word if you are able to iterate trough the whole string you grabed append to list anw
+        List<String> anw =  new ArrayList<>();
         for (int i = 0;i< list.size(); i++ ){
-            for (int j = i+1; j< list.)
+            for (int j = i+1; j< list.size(); j++){
+                try{
+                    if(list.get(i).equals(list.get(j).substring(0,list.get(i).length()))){
+                        anw.add(list.get(j));
+                    }
+                } catch (Exception e){
+
+                }
+            }
         }
+        return anw;
     }
 }
--- week2/problem3/src/main/java/com/revature/problem3/Solution.java
+++ week2/problem3/src/main/java/com/revature/problem3/Solution.java
@@ -4,6 +4,25 @@ import java.util.*;
 
 public class Solution {
     public String wordCount(String sentence) {
+        // simple dictionar
+        // go through list if word not in dictonary add and appen one if in dict then lookup and val +1
+        Map<String, Integer> count = new HashMap<>();
+
+        StringBuilder word = new StringBuilder();
+
+        for (int i  = 0; i < sentence.length(); i++){
+            if(sentence.charAt(i)  == ' '){
+                if (count.containsKey(word.toString())){
+                    count.put(word.toString(),count.get(word.toString()) +1);
+                } else {
+                    count.put(word.toString(),1);
+                }
+                word.setLength(0);
+            }else{
+                word.append(sentence.substring(i));
+            }
+        }
+
         throw new RuntimeException("TODO! Needs implementation...");
     }
 }

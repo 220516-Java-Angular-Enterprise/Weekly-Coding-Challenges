9f36b1d9373109f8682a676eb17742395c05531c
Brayan <baviles599@gmail.com>
2022-06-04 19:54:01 -0500

    Bao solution

--- week3/problem2/src/main/java/com/revature/problem/Solution.java
+++ week3/problem2/src/main/java/com/revature/problem/Solution.java
@@ -1,9 +1,17 @@
 package com.revature.problem;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public boolean ifNDouleExist(int[] arr) {
+        List<Integer> check = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for(int i : check){
+            if(check.contains(i*2)) return true;
+        }
         Arrays.sort(arr);
          for(int i =arr.length-1; i>0; i--){
              if(arr[arr.length-1] == arr[i]*2)
